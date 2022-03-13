6aa6c30b86992710f3fb03c348795f257bf3fca6
lubod <lubo.daniel.dilov@gmail.com>
2022-03-13 18:01:22 +0200

    Added Actors Controller

---
+++ Cinemahub_Official/Data/Migrations/20220313155807_Initial_setup_1.2.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Cinemahub_Official.Data.Migrations
+{
+    public partial class Initial_setup_12 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Actor",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(nullable: true),
+                    Birthday = table.Column<DateTime>(nullable: false),
+                    Nationality = table.Column<string>(nullable: true),
+                    Status = table.Column<bool>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Actor", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Actor");
+        }
+    }
+}
