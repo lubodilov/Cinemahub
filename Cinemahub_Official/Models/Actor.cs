5da0e068ec32389fef3bc4650f1ca654db926ac9
lubod <lubo.daniel.dilov@gmail.com>
2022-03-13 17:53:18 +0200

    Actor Model added

---
+++ Cinemahub_Official/Models/Actor.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Cinemahub_Official.Models
+{
+    public class Actor
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public DateTime Birthday { get; set; }
+        public string Nationality { get; set; }
+        public bool Status { get; set; }
+        //public List<Award> Awards { get; set; }
+        //public Movie[] Movies { get; set; }
+
+        public Actor()
+        {
+
+        }
+    }
+}
