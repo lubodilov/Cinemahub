8294a1f931ab0d80630c1588fce0b51f2245a53a
lubod <lubo.daniel.dilov@gmail.com>
2022-03-18 18:15:58 +0200

    added Actors validation

--- Cinemahub_Official/Models/Actor.cs
+++ Cinemahub_Official/Models/Actor.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,8 +9,15 @@ namespace Cinemahub_Official.Models
     public class Actor
     {
         public int Id { get; set; }
+        [StringLength(30, MinimumLength = 1)]
+        [Required]
         public string Name { get; set; }
+        
+        [Required]
         public DateTime Birthday { get; set; }
+        
+        [StringLength(30, MinimumLength = 1)]
+        [Required]
         public string Nationality { get; set; }
         public bool Status { get; set; }
         //public List<Award> Awards { get; set; }
