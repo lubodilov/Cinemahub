841351a4d1d4d9bab8d0dc28ca732638cff05ba1
ouplex <momchil26@gmail.com>
2022-03-18 20:44:10 +0200

    Add Awards, Privacy

---
+++ Cinemahub_Official/Controllers/AwardsController.cs
@@ -0,0 +1,180 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Cinemahub_Official.Data;
+using Cinemahub_Official.Models;
+
+namespace Cinemahub_Official.Controllers
+{
+    public class AwardsController : Controller
+    {
+        private readonly Application1DbContext _context;
+
+        public AwardsController(Application1DbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Awards
+        //public async Task<IActionResult> Index()
+        //{
+        //    return View(await _context.Awards.ToListAsync());
+        //}
+
+        public async Task<IActionResult> Index(string searchString, string awardLocation)
+        {
+            IQueryable<string> locationQuery = from m in _context.Awards
+                                               orderby m.Location
+                                               select m.Location;
+            var awards = from m in _context.Awards
+                         select m;
+
+            if (!string.IsNullOrEmpty(searchString))
+            {
+                awards = awards.Where(s => s.Name!.Contains(searchString));
+            }
+
+            if (!string.IsNullOrEmpty(awardLocation))
+            {
+                awards = awards.Where(x => x.Location == awardLocation);
+            }
+
+            var AwardsLocationVM = new AwardsNameViewModel
+            {
+                Location = new SelectList(await locationQuery.Distinct().ToListAsync()),
+                Awards = await awards.ToListAsync()
+            };
+
+            return View(AwardsLocationVM);
+        }
+
+        // GET: Awards/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var awards = await _context.Awards
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (awards == null)
+            {
+                return NotFound();
+            }
+
+            return View(awards);
+        }
+
+        // GET: Awards/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Awards/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Name,Founded,Location,WebSite")] Awards awards)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(awards);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(awards);
+        }
+
+        // GET: Awards/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var awards = await _context.Awards.FindAsync(id);
+            if (awards == null)
+            {
+                return NotFound();
+            }
+            return View(awards);
+        }
+
+        // POST: Awards/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Name,Founded,Location,WebSite")] Awards awards)
+        {
+            if (id != awards.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(awards);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!AwardsExists(awards.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(awards);
+        }
+
+        // GET: Awards/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var awards = await _context.Awards
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (awards == null)
+            {
+                return NotFound();
+            }
+
+            return View(awards);
+        }
+
+        // POST: Awards/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var awards = await _context.Awards.FindAsync(id);
+            _context.Awards.Remove(awards);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool AwardsExists(int id)
+        {
+            return _context.Awards.Any(e => e.Id == id);
+        }
+    }
+}
---
+++ Cinemahub_Official/Migrations/20220318163306_Initial setup 1.5.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Cinemahub_Official.Migrations
+{
+    public partial class Initialsetup15 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Movies",
+                maxLength: 60,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Director",
+                table: "Movies",
+                maxLength: 60,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.CreateTable(
+                name: "Awards",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(nullable: true),
+                    Founded = table.Column<DateTime>(nullable: false),
+                    Location = table.Column<string>(nullable: true),
+                    WebSite = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Awards", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Awards");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Movies",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldMaxLength: 60);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Director",
+                table: "Movies",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldMaxLength: 60);
+        }
+    }
+}
